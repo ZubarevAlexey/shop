8cd54affef32c8d68c64a0442a150927b1807a52
User <stparchkg@gmail.com>
2023-10-06 17:04:27 +0600

    создал baseDto

---
+++ src/main/java/kg/java/shop/core/models/dtos/base/BaseDto.java
@@ -0,0 +1,14 @@
+package kg.java.shop.core.models.dtos.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public abstract class BaseDto {
+    private Long id;
+}
