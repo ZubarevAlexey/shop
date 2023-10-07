70c4027ef1d526268738481f6476566430f643f9
User <stparchkg@gmail.com>
2023-10-07 11:51:48 +0600

    создал абстракции фасада для purchase

---
+++ src/main/java/kg/java/shop/core/contracts/facades/IPurchaseFacade.java
@@ -0,0 +1,17 @@
+package kg.java.shop.core.contracts.facades;
+
+import kg.java.shop.core.exceptions.EntityDuplicateExceptions;
+import kg.java.shop.core.exceptions.EntityNotFoundException;
+import kg.java.shop.core.models.dtos.product.*;
+import kg.java.shop.core.models.dtos.purchase.*;
+import org.springframework.http.HttpStatus;
+
+public interface IPurchaseFacade {
+    PurchaseDto register(CreatedPurchaseDto model) throws EntityDuplicateExceptions;
+
+    PurchaseDto update(UpdatedPurchaseDto model) throws EntityNotFoundException;
+
+    HttpStatus delete(DeletePurchaseDto model) throws EntityNotFoundException;
+
+    PurchaseDto getById(FindByIdPurchaseDto model) throws EntityNotFoundException;
+}
