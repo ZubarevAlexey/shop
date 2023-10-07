6455bc9460662c3acf1f1b5293b8364c5ebddab7
User <stparchkg@gmail.com>
2023-10-07 12:43:44 +0600

    создал controller для product

--- src/main/java/kg/java/shop/controllers/v1/ProductV1Controller.java
+++ src/main/java/kg/java/shop/controllers/v1/ProductV1Controller.java
@@ -1,56 +1,46 @@
 package kg.java.shop.controllers.v1;
 
-import kg.java.shop.core.contracts.facades.IBuyerFacade;
+import kg.java.shop.core.contracts.facades.IProductFacade;
+import kg.java.shop.core.contracts.facades.IPurchaseFacade;
+import kg.java.shop.core.contracts.services.IProductService;
 import kg.java.shop.core.exceptions.EntityDuplicateExceptions;
 import kg.java.shop.core.exceptions.EntityNotFoundException;
-import kg.java.shop.core.models.dtos.buyer.*;
+import kg.java.shop.core.models.dtos.product.*;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/buyer")
-public class BuyerV1Controller {
+@RequestMapping("/api/v1/product")
+public class ProductV1Controller {
 
-    private final IBuyerFacade buyerFacade;
 
-    public BuyerV1Controller(IBuyerFacade buyerFacade) {
-        this.buyerFacade = buyerFacade;
+    private final IProductFacade productFacade;
+
+    public ProductV1Controller(IProductFacade productFacade) {
+        this.productFacade = productFacade;
     }
 
-    @PostMapping("/signup")
-    public ResponseEntity<BuyerDto> register(@RequestBody CreatedBuyerDto model){
+    @PostMapping("/add")
+    public ResponseEntity<ProductDto> add(@RequestBody CreatedProductDto model){
         try {
-            return ResponseEntity.ok(buyerFacade.register(model));
+            return ResponseEntity.ok(productFacade.register(model));
         }
         catch (EntityDuplicateExceptions e){
-            return ResponseEntity.badRequest().build();
-        }
-        catch (Exception e){
             return ResponseEntity.notFound().build();
         }
+        catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
     }
-
     @PostMapping("/update")
-    public ResponseEntity<BuyerDto> update(@RequestBody UpdatedBuyerDto model){
+    public ResponseEntity<ProductDto> add(@RequestBody UpdatedProductDto model){
         try {
-            return ResponseEntity.ok(buyerFacade.update(model));
-        }
-        catch (EntityNotFoundException e){
-            return ResponseEntity.notFound().build();
-        }
-        catch (Exception e){
-            return ResponseEntity.badRequest().build();
-        }
-    }
-    @PostMapping("/delete")
-    public ResponseEntity<HttpStatus> delete(@RequestBody DeleteBuyerDto model){
-        try {
-            return ResponseEntity.ok(buyerFacade.delete(model));
+            return ResponseEntity.ok(productFacade.update(model));
         }
         catch (EntityNotFoundException e){
             return ResponseEntity.notFound().build();
@@ -60,16 +50,28 @@ public class BuyerV1Controller {
         }
     }
 
-    @PostMapping("/id")
-    public ResponseEntity<BuyerDto> getById(@RequestBody FindByIdBuyerDto model){
+    @PostMapping("/delete")
+    public ResponseEntity<HttpStatus> delete(@RequestBody DeleteProductDto model){
         try {
-            return ResponseEntity.ok(buyerFacade.getById(model));
+            return ResponseEntity.ok(productFacade.delete(model));
         }
         catch (EntityNotFoundException e){
             return ResponseEntity.notFound().build();
         }
         catch (Exception e){
             return ResponseEntity.badRequest().build();
+        }
+    }
+    @PostMapping("/id")
+    public ResponseEntity<ProductDto> getById(@RequestBody FindByIdProductDto model){
+        try {
+            return ResponseEntity.ok(productFacade.getById(model));
+        }
+        catch (EntityNotFoundException e){
+            return ResponseEntity.notFound().build();
+        }
+        catch (Exception e){
+            return ResponseEntity.internalServerError().build();
         }
     }
 }
