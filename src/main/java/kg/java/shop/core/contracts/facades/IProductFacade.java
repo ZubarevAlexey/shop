d5cda47bb55af375ae3621ff4c874e3ae2116401
User <stparchkg@gmail.com>
2023-10-07 11:50:16 +0600

    создал абстракции фасада для product

---
+++ src/main/java/kg/java/shop/core/contracts/facades/IProductFacade.java
@@ -0,0 +1,15 @@
+package kg.java.shop.core.contracts.facades;
+
+import kg.java.shop.core.exceptions.EntityDuplicateExceptions;
+import kg.java.shop.core.exceptions.EntityNotFoundException;
+import kg.java.shop.core.models.dtos.buyer.*;
+import kg.java.shop.core.models.dtos.product.*;
+import org.springframework.http.HttpStatus;
+
+public interface IProductFacade {
+
+    ProductDto register(CreatedProductDto model) throws EntityDuplicateExceptions;
+    ProductDto update(UpdatedProductDto model)throws EntityNotFoundException;
+    HttpStatus delete(DeleteProductDto model) throws EntityNotFoundException;
+    ProductDto getById(FindByIdProductDto model) throws EntityNotFoundException;
+}
